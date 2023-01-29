aa8cd83291094c8b91d42aab57a68af2bf12b520
stephenwalker8311 <122221670+stephenwalker8311@users.noreply.github.com>
2023-01-29 22:57:47 +0000

    updates

--- index.js
+++ index.js
@@ -1,55 +1,9 @@
 
-var app = require('express')
-var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json()
+import { post } from 'express'
+import { json } from 'body-parser'
+var jsonParser = json()
 
-exports.handler = async function http(req) {
-
-  let html = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
-  </head>
-  <body>
-
-    <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
-    </h1>
-
-    <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
-
-  </body>
-</html>`
-
-  return {
-    headers: {
-      'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
-    statusCode: 200,
-    body: html
-  }
-}
-
-// Other example responses
-
-// Forward requester to a new path
-exports.handler = async function http (req) {
-
-  return {
-    statusCode: 302,
-    headers: {'location': '/about'}
-  }
-}
-
-app.post('/api/users', jsonParser, function (req, res) {
+post('/api/users', jsonParser, function (req, res) {
 
 })
 
